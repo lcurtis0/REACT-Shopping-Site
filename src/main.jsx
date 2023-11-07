9e33023265c5dc20efceb673be046bbe7d1a5893
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-11-07 10:35:21 -0500

    Removed About + Added Error and Toys page + set up main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,46 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App.jsx'
-import SearchBooks from './pages/SearchBooks'
-import SavedBooks from './pages/SavedBooks'
+import Error from './src/pages/Error';
+
+import Home from './src/pages/Home';
+import Food from './src/pages/Food';
+import Electronics from './src/pages/Electronics';
+import HouseSupplies from './src/pages/HouseSupplies';
+import Toys from './src/pages/Toys';
+import Books from './src/pages/Books';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <Error />,
     errorElement: <h1 className='display-2'>Wrong page!</h1>,
     children: [
       {
         index: true,
-        element: <SearchBooks />
-      }, {
-        path: '/saved',
-        element: <SavedBooks />
-      }
+        element: <Home />,
+      },
+      {
+        path: '/Food',
+        element: <Food />,
+      },
+      {
+        path: '/Electronics',
+        element: <Electronics />,
+      },
+      {
+        path: '/HouseSupplies',
+        element: <HouseSupplies />,
+      },
+      {
+        path: '/Toys',
+        element: <Toys />,
+      },
+      {
+        path: '/Books',
+        element: <Books />,
+      },
     ]
   }
 ])
---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+        <hr></hr>
+        <h2>Feel free to look around</h2>
+  
+        <p>
+  
+        </p>
+  
+        <img
+          alt="Sea shell"
+          className="image"
+          src="Assets/images/shell-1.png"
+        />
+  
+        <p>
+  
+        </p>
+  
+        <p>
+  
+        </p>
+  
+        <p>
+  
+        </p>
+      </div>
+    );
+  }
